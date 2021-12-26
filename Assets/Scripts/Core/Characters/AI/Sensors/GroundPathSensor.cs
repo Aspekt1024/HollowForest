f5652284dc5ecffcffb44a46e0fe2fa4de26376a
Dan Szabo <d.szabo87@gmail.com>
2021-12-26 22:13:03 +1100

    Add wall sensors

--- Assets/Scripts/Core/Characters/AI/Sensors/GroundPathSensor.cs
+++ Assets/Scripts/Core/Characters/AI/Sensors/GroundPathSensor.cs
@@ -8,9 +8,11 @@ namespace HollowForest.AI
     {
         public Collider2D leftGroundCollider;
         public Collider2D rightGroundCollider;
+        public Collider2D leftWallCollider;
+        public Collider2D rightWallCollider;
 
         private readonly List<Collider2D> colliders = new List<Collider2D>();
-        
+
         protected override void OnInit()
         {
             
@@ -18,11 +20,15 @@ namespace HollowForest.AI
 
         private void Update()
         {
-            var numContacts = leftGroundCollider.GetContacts(colliders);
-            agent.memory.SetState(AIState.IsNearLeftEdge, numContacts == 0);
+            var hasLeftGround = leftGroundCollider.GetContacts(colliders) > 0;
+            var hasLeftWall = leftWallCollider.GetContacts(colliders) > 0;
+            var isLeftPathObstructed = !hasLeftGround || hasLeftWall;
+            agent.memory.SetState(AIState.IsNearLeftEdge, isLeftPathObstructed);
             
-            numContacts = rightGroundCollider.GetContacts(colliders);
-            agent.memory.SetState(AIState.IsNearRightEdge, numContacts == 0);
+            var hasRightGround = rightGroundCollider.GetContacts(colliders) > 0;
+            var hasRighttWall = rightWallCollider.GetContacts(colliders) > 0;
+            var isRightPathObstructed = !hasRightGround || hasRighttWall;
+            agent.memory.SetState(AIState.IsNearRightEdge, isRightPathObstructed);
         }
     }
 }
